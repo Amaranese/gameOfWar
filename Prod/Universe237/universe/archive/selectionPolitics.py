522463ec7fe066ec86431c25409ef57171165a9f
Amaranese <alejandromaranes@gmail.com>
2022-05-05 11:32:24 +0200

    Create selectionPolitics.py

---
+++ Prod/Universe237/universe/archive/selectionPolitics.py
@@ -0,0 +1,27 @@
+# All WAR Menu Function
+
+from gameConquest_utilities import slow_print as slow_print
+from gameConquest_utilities import med_print as med_print
+from gameConquest_utilities import fast_print as fast_print
+from gameConquest_utilities import superfast_print as superfast_print
+from gameConquest_utilities import clearScreen as clearScreen
+from gameConquest_utilities	 import preferencePrint as preferencePrint
+
+"""
+# =====================================================================
+# =====================================================================
+# =====================================================================
+#                           WAR  MENU
+#     1. Gamble
+#     2. Trade
+#     2.1 buy
+#     2.2 sell
+#     5. Exit
+# =====================================================================
+# =====================================================================
+# =====================================================================
+"""
+
+def politicalCabinet(myNation,year):
+	fast_print('This feature is not ready yet...')
+	return(myNation)
